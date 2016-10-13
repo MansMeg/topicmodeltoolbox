ceed76bb9b0d9bfdddc73e6436bc06fc81592b22
Måns Magnusson <mons.magnusson@gmail.com>
2016-10-13 15:52:05 -0400

    Added test suite

---
+++ tests/testthat/test-topic_checking.R
@@ -0,0 +1,14 @@
+context("type_topic_reweighting")
+
+load(system.file("extdata/sotu_50.Rdata", package = "topicmodeltoolbox"))
+
+test_that(desc="imi",{
+  imi_test <- imi(sotu50)
+  expect_equal(imi_test$imi[1:2], c(0.2901117, 0.7248372), tolerance = .00001)
+})
+
+test_that(desc="mi",{
+  mi_test <- mi(sotu50)
+  expect_equal(mi_test$mi[1:2], c(3.333453, 3.436727), tolerance = .00001)
+})
+
